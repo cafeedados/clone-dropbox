46e68f6596b30f645aed35d688aa47a08c0192e3
Pedro Lourenco <dev.pedrolourenco@gmail.com>
2020-02-17 16:33:06 -0300

    Barra de progresso

--- app/public/src/controller/DropboxController.js
+++ app/public/src/controller/DropboxController.js
@@ -5,7 +5,11 @@ class DropboxController {
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files'); //input files
         this.snackModalEl  = document.querySelector('#react-snackbar-root'); //modal de carregamento do arquivo
-        
+        this.progressiBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')//barrinha
+        this.namefileEl = this.snackModalEl.querySelector('.filename');//nome do arquivo na barrinha
+        this.timeleftEl = this.snackModalEl.querySelector('.timeleft');//tempo restante
+
+
         this.initEvents();
 
     };
@@ -23,11 +27,23 @@ class DropboxController {
             
             this.uploadTask(event.target.files); //o evento e o change o taget e qual o alvo ou seja pegar o elemento do input e o files que sao os arquivos selecionados
             
-            
-            this.snackModalEl.style.display = 'block';
+            this.modalShow();
+
+            this.inputFilesEl.value = '';
+
         });
 
     };//final initEvents
+
+    modalShow(show = true){
+
+        this.snackModalEl.style.display = (show) ? 'block' : 'none';
+
+
+    };//fim modal Show
+
+
+
 
     uploadTask(files){
 
@@ -41,6 +57,9 @@ class DropboxController {
                 ajax.open('POST', '/upload');
 
                 ajax.onload = event => {
+
+                    this.modalShow(false);
+
                     try{
                         resolve(JSON.parse(ajax.responseText))
                     } catch(e) {
@@ -49,17 +68,77 @@ class DropboxController {
                 };
 
                 ajax.onerror = event =>{
+                    this.modalShow(false);
                     reject(event);
+                };
+
+                //para pegar em tempo real o progresso
+                ajax.upload.onprogress = event => {
+
+                    this.uploadProgress(event, file);
+                    //console.log(event);
                 };
 
                 let formData = new FormData();
 
                 formData.append('input-file', file)
+
+                this.startUploadTime = Date.now();
+
                 ajax.send(formData);
 
               }));
         });
 
         return Promise.all(promisses);
-    }; //final upload tak
+    }; //final upload taks
+
+    uploadProgress(event, file){
+
+        let timespent = Date.now() - this.startUploadTime;
+        let loaded = event.loaded;
+        let total = event.total;
+        let porcent = parseInt((loaded / total) * 100);
+        let timeleft = ((100 - porcent) * timespent) / porcent;
+
+        this.progressiBarEl.style.width = `${porcent}%`
+
+        this.namefileEl.innerHTML = file.name;
+        this.timeleftEl.innerHTML = this.formatTimeToHuman(timeleft);
+
+        console.log(timespent, timeleft, porcent);
+
+
+
+
+    };//final upload progressive
+
+    formatTimeToHuman(duration){
+
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);;
+
+        if (hours > 0){
+            return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+        };
+
+        if (minutes > 0){
+            return `${minutes} minutos e ${seconds} segundos`;
+
+        }
+
+        if (seconds > 0){
+            return `${seconds} segundos`;
+
+        }
+
+        return ' ';
+
+
+
+
+    }//final formatTimeToHuman
+
+
 }; 
--- app/readme.md
+++ app/readme.md
@@ -1,3 +1,20 @@
-Para tratar uploads de arquivos usamos o 
+## Recebendo arquivos com Node.JS (formidable)
 
-formidable --save
+Para tratar uploads de arquivos usamos o formidable e para instalar ele basta fazer
+
+```
+$ npm install formidable --save
+```
+
+## Atualizando a View com informacoes do Progresso do Upload
+
+Atualizar o modal que realiza aquelas informacoes de upload
+e trabalhando com 3 informacoes
+
+1. nome do arquivo
+
+2. barra de progresso
+
+3. tempo de duracao
+
+para simular uma conexao lenta vou no chorme coloco a compatibilidade mobile e clico em low-end-mobile
